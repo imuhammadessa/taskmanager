895a0d03dba7af6727cfb189d675a56d405bc306
Muhammad Essa <iMuhammadessa@gmail.com>
2026-05-09 17:26:03 +0500

    updated files for database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {});
-        console.log('MongoDB Connected...');
-    } catch (err) {
-        console.error('Error connecting to MongoDB:', err.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
