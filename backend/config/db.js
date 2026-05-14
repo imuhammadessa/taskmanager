81afc90b31d4915ffeafcef68ba331239c822c92
Muhammad Essa <iMuhammadessa@gmail.com>
2026-05-14 15:03:24 +0500

    fixed: database_connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-
-    console.log("MongoDB Connected...");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected`);
   } catch (err) {
     console.error("Error connecting to MongoDB:", err.message);
     process.exit(1);
