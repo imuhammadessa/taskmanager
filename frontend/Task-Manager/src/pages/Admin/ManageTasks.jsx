177eed267637a10767cd89af8cdb48d240eaeadf
Muhammad Essa <iMuhammadessa@gmail.com>
2026-05-17 17:32:35 +0500

    Initial commit

--- frontend/Task-Manager/src/pages/Admin/ManageTasks.jsx
+++
@@ -1,9 +0,0 @@
-const ManageTasks = () => {
-  return (
-    <div>
-      <h1>Manage Tasks</h1>
-    </div>
-  );
-};
-
-export default ManageTasks;
--- frontend/Task-Manager/src/routes/PrivateRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-const PrivateRoute = ({ allowedRoles }) => {
-  return <Outlet allowedRoles={allowedRoles} />;
-};
-
-export default PrivateRoute;
